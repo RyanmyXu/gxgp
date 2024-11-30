05b1571707684157be9136a903707748ce0893fa
Ryan <Ryan@gmail.com>
2024-12-01 00:31:03 +0800

    提交sql

--- 传染病对照表导入sql生成.py
+++ 传染病对照表导入sql生成.py
@@ -13,4 +13,4 @@ print(mapp.columns[0])
 
 
 for index,i in enumerate(mapp[mapp.columns[0]]):
-    print( f"updata hbos_business.mapping set third_code='{mapp["third_code"][index]}' third_name='{mapp["third_name"][index]}' where his_name='{mapp["his_name"][index]}' and org_id='20044001' and is_deleted=0")
+    print( f"update hbos_business.mapping set third_code='{mapp["third_code"][index]}' third_name='{mapp["third_name"][index]}' where his_name='{mapp["his_name"][index]}' and org_id='20044001' and is_deleted=0")
