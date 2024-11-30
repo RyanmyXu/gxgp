db3d0d0da7476131fab5fbb6cfc4a61f8e86106d
Ryan <Ryan@gmail.com>
2024-12-01 00:19:18 +0800

    提交sql

--- 传染病对照表导入sql生成.py
+++ 传染病对照表导入sql生成.py
@@ -1,5 +1,13 @@
+from dask.config import update
+
 import config
 import pandas as pd
 
-myex=pd.read_excel('/Users/ryan/Desktop/mapping.xlsx')
-print(myex)
+mapp=pd.read_excel('/Users/ryan/Desktop/mapping.xlsx')
+
+print(f"shape:{mapp.shape}" )
+print(mapp.columns[0])
+
+
+for index,i in enumerate(mapp[mapp.columns[0]]):
+    print( f"updata hbos_business.mapping set third_code='{mapp["third_code"][index]}' third_name='{mapp["third_name"][index]}' where his_name='{mapp["his_name"][index]}' and org_id='20044001' and is_deleted=0")
